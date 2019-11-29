6089a34f472d4502a5cf9299ad7fbb5a1fad5390
Liam Clarke <liam.clarke.94@outlook.com>
2019-11-29 17:53:46 +0000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Liam Clarke`,
+    description: `Digital Developer. Developing everything Digital.`,
+    author: `Liam Clarke`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
