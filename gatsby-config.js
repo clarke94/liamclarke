cf3d6d179b36e951eea6e642e0379ee2f0091070
Liam Clarke <liam.clarke.94@outlook.com>
2019-12-01 19:32:52 +0000

    Fix manifest icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,13 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Liam Clarke`,
+                short_name: `liamclarke`,
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `static/favicon/favicon-32x32.png`, // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout/layout"
-import Image from "../components/image/image"
 import SEO from "../components/seo/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+    <Layout>
+        <SEO title="Home" />
 
-  </Layout>
+    </Layout>
 )
 
 export default IndexPage
