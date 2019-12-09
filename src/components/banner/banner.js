cdda524afabc4e212390c3b3d2945308d85d468c
Liam Clarke <liam.clarke.94@outlook.com>
2019-12-09 23:46:01 +0000

    Update Banner typography

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 import style from './banner.module.scss';
 
 export const Banner = () => (
     <section className={style.banner}>
         <Container>
-            <h1 className={style.banner__heading}>Digital Developer</h1>
-            <h2 className={style.banner__subheading}>
-                Lorem ipsum dolor sit amet.
-                <span>consectetur, adipiscing, elit</span>
-                .
-            </h2>
+            <Typography variant="h2" component="h1" className={style.banner__heading}>Digital Developer</Typography>
+            <Typography variant="subtitle1" component="h2" className={style.banner__subheading}>Lorem ipsum dolor sit amet. consectetur, adipiscing, elit</Typography>
         </Container>
     </section>
 );
