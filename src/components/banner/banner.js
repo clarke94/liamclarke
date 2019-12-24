1996637ec85f1520fd67c111b571e59223d968e8
Liam Clarke <liam.clarke.94@outlook.com>
2019-12-24 16:03:48 +0000

    Add Banner typography loop

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Container, Typography } from '@material-ui/core';
+import anime from 'animejs';
 import style from './banner.module.scss';
 
-export const Banner = () => (
-    <section className={style.banner}>
-        <Container>
-            <Typography variant="h2" component="h1" className={style.banner__heading}>Digital Developer</Typography>
-            <Typography variant="subtitle1" component="h2" className={style.banner__subheading}>Lorem ipsum dolor sit amet. consectetur, adipiscing, elit</Typography>
-        </Container>
-    </section>
-);
+export const Banner = () => {
+
+    const classSector = style.banner_sector;
+    console.log(classSector);
+
+    anime.timeline({loop: true})
+        .add({
+            targets: classSector,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 600,
+        })
+
+    
+    return (
+        <section className={style.banner}>
+            <Container>
+                <Typography variant="h2" component="h1">
+                    <span className={style.banner_sector}>Digital </span>
+                    <span className={style.banner_sector}>Web </span>
+                    <span className={style.banner_sector}>App </span>
+                    <span className={style.banner_sector}>Mobile </span>
+                    <span className={style.banner_role}>Developer</span>
+                </Typography>
+                <Typography variant="subtitle1" component="h2">Lorem ipsum dolor sit amet. consectetur, adipiscing, elit</Typography>
+            </Container>
+        </section>
+    );
+}
+
 
 export default Banner;
