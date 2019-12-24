c6facedeca6fecdcfbb5761df3bdfcb243222a51
Liam Clarke <liam.clarke.94@outlook.com>
2019-12-24 12:41:40 +0000

    Add Footer component social links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons';
 import style from './footer.module.scss';
 
 export const Footer = () => (
     <footer className={style.footer}>
-        ©
-            {new Date().getFullYear()}
-        , liamclarke.dev
+        <Container classes={{root: style.footer_container}}>
+            <a href="https://www.linkedin.com/in/liamclarke94" className={style.footer_link} target="_blank" rel="noopener" aria-label="Liam Clarke's LinkedIn profile">
+                <FontAwesomeIcon icon={faLinkedin} size="3x"></FontAwesomeIcon>
+            </a>
+            <a href="https://github.com/clarke94" className={style.footer_link} target="_blank" rel="noopener" aria-label="Liam Clarke's Github profile">
+                <FontAwesomeIcon icon={faGithub} size="3x"></FontAwesomeIcon>
+            </a>
+            <a href="https://codepen.io/Clarke2509" className={style.footer_link} target="_blank" rel="noopener" aria-label="Liam Clarke's Codepen profile">
+                <FontAwesomeIcon icon={faCodepen} size="3x"></FontAwesomeIcon>
+            </a>
+        </Container>
+        <Container classes={{root: style.footer_container}}>
+            <span>©&nbsp;</span>
+            <span>{new Date().getFullYear()}</span>
+            <span>, liamclarke.dev</span>
+        </Container>
     </footer>
 );
 
