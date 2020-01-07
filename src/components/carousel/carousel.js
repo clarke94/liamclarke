329a072eb3496a1ccc7719c9714f0acf32ca1554
Liam Clarke <liam.clarke.94@outlook.com>
2020-01-07 21:20:01 +0000

    Add project carousel arrows

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,9 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from '@material-ui/core';
 import Slider from 'react-slick';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import style from './carousel.module.scss';
+
+const CarouselArrows = (props) => {
+    const { icon, onClick, modifier } = props;
+    const variant = modifier === 'left' ? style.carousel__button_left : style.carousel__button_right;
+    return (
+        <button type="button" className={`${style.carousel__button} ${variant}`} onClick={onClick}>
+            <FontAwesomeIcon icon={icon} size="2x" className={style.carousel__arrow} />
+        </button>
+    );
+};
+
+CarouselArrows.propTypes = {
+    icon: PropTypes.objectOf(PropTypes.any).isRequired,
+    onClick: PropTypes.func,
+    modifier: PropTypes.string.isRequired,
+};
+
+CarouselArrows.defaultProps = {
+    onClick: undefined,
+};
 
 export const Carousel = (props) => {
     const { slides } = props;
@@ -13,7 +35,7 @@ export const Carousel = (props) => {
         const url = `url(${slide.childImageSharp.fluid.src})`;
 
         return (
-            <div className={style.carousel__container}>
+            <div key={slide.childImageSharp.fluid.src} className={style.carousel__container}>
                 <div className={style.carousel__image} style={{ backgroundImage: url }} />
             </div>
         );
@@ -21,7 +43,15 @@ export const Carousel = (props) => {
 
     return (
         <Container component="section">
-            <Slider slidesToShow={1} slidesToScroll={1} speed={500} infinite dots arrows={false}>
+            <Slider
+                slidesToShow={1}
+                slidesToScroll={1}
+                speed={500}
+                prevArrow={<CarouselArrows icon={faChevronCircleLeft} modifier="left" />}
+                nextArrow={<CarouselArrows icon={faChevronCircleRight} modifier="right" />}
+                infinite
+                dots
+            >
                 {allSlides}
             </Slider>
         </Container>
