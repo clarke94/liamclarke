f193dce79e835d8130575be79afc1f73e94c9c21
Liam Clarke <liam.clarke.94@outlook.com>
2020-04-09 18:07:30 +0100

    Remove react reveal

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Container } from '@material-ui/core';
-import { Link } from 'gatsby';
 
 export const Header = () => (
     <header>
         <Container>
             <Typography variant="h6" component="span" color="inherit">
-                <Link to="/">Liam Clarke</Link>
+                <a href="/">Liam Clarke</a>
             </Typography>
         </Container>
     </header>
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Typography } from '@material-ui/core';
-import Fade from 'react-reveal/Fade';
 import PropTypes from 'prop-types';
 import style from './section.module.scss';
 
@@ -8,12 +7,10 @@ export const Section = (props) => {
     const { children, title } = props;
 
     return (
-        <Fade bottom>
-            <Container component="section" classes={{ root: style.section }}>
-                {title ? <Typography variant="h4" component="h2" align="center" display="block" color="primary" gutterBottom>{title}</Typography> : ''}
-                {children}
-            </Container>
-        </Fade>
+        <Container component="section" classes={{ root: style.section }}>
+            {title ? <Typography variant="h4" component="h2" align="center" display="block" color="primary" gutterBottom>{title}</Typography> : ''}
+            {children}
+        </Container>
     );
 };
 
